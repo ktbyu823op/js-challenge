eccfd1a7e202d4a2880704340daab2d1af2e1328
ktbyu823op <ktb823op@naver.com>
2024-02-19 23:16:22 +0900

    Added Move directly to website Function

--- js/search.js
+++ js/search.js
@@ -6,23 +6,29 @@ const searchChangeBtn = document.querySelector(".search__change-btn");
 function onSearchSubmit(event) {
   event.preventDefault();
 
-  if (searchInput.classList.contains("search__input--google")) {
-    searchOnGoogle();
+  const searchWords = searchInput.value;
+  searchInput.value = "";
+  if (searchWords.includes(".")) {
+    moveToSite(searchWords);
   } else {
-    searchOnNaver();
+    if (searchInput.classList.contains("search__input--google")) {
+      searchOnGoogle(searchWords);
+    } else {
+      searchOnNaver(searchWords);
+    }
   }
 }
 
-function searchOnGoogle() {
-  const searchWords = searchInput.value;
-  searchInput.value = "";
+function moveToSite(webName) {
+  location.href = `https://www.${webName}`;
+}
+
+function searchOnGoogle(searchWords) {
   const googleUrl = `https://www.google.com/search?q=${searchWords}`;
   location.href = googleUrl;
 }
 
-function searchOnNaver() {
-  const searchWords = searchInput.value;
-  searchInput.value = "";
+function searchOnNaver(searchWords) {
   const naverUrl = `https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${searchWords}`;
   location.href = naverUrl;
 }
